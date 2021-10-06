9a4cf605d4b3f4910dc0377b18584e409b518794
MatheusFelipeMarinho <matheus_fellip@hotmail.com>
2021-10-06 13:24:23 -0300

    Implementar Municipio de Governador Valadadres

---
+++ src/Counties/M3127701/Convert.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace NFePHP\NFSe\Counties\M3127701;
+
+/**
+ * Classe a construção do xml da NFSe
+ * para a Cidade de Governador Valadares - MG
+ * conforme o modelo SIGISS
+ *
+ * @category  NFePHP
+ * @package   NFePHP\NFSe\Counties\M3127701\Convert
+ * @copyright NFePHP Copyright (c) 2016
+ * @license   http://www.gnu.org/licenses/lgpl.txt LGPLv3+
+ * @license   https://opensource.org/licenses/MIT MIT
+ * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
+ * @author    Matheus Felipe Marinho matheus_fellip@hotmail.com
+ * @link      http://github.com/nfephp-org/sped-nfse for the canonical source repository
+ */
+
+use NFePHP\NFSe\Models\SIGISS\Convert as ConvertSIGISS;
+
+class Convert extends ConvertSIGISS
+{
+}

6f0d6f9b7a3b9f54fce03ea27f6652817f128f6c
Tiago Franco <tiago.ffx@gmail.com>
2022-02-10 01:26:56 -0300

    integracao cidade de bage RS

---
+++ src/Counties/M4301602/Convert.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace NFePHP\NFSe\Counties\M4301602;
+
+/**
+ * Classe a construção do xml da NFSe
+ * para a Cidade de bAGE - RS
+ * conforme o modelo ABRASF
+ *
+ * @category  NFePHP
+ * @package   NFePHP\NFSe\Counties\M3131703\Convert
+ * @copyright NFePHP Copyright (c) 2016
+ * @license   http://www.gnu.org/licenses/lgpl.txt LGPLv3+
+ * @license   https://opensource.org/licenses/MIT MIT
+ * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
+ * @author    Lucas B. Simões <lucas_development at outlook dot com>
+ * @link      http://github.com/nfephp-org/sped-nfse for the canonical source repository
+ */
+
+use NFePHP\NFSe\Models\Abrasf\Convert as ConvertAbrasf;
+
+class Convert extends ConvertAbrasf
+{
+}

122f1f03b50a63e55ed2d10f59207928072e7090
Vagner Kaefer <vagner@kaefer.eng.br>
2026-05-27 09:56:40 -0300

    Adicionando São Lourenço do Oeste SC (IBGE 4216909) (#16)

---
+++ src/Counties/M4216909/Convert.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace NFePHP\NFSe\Counties\M4216909;
+
+/**
+ * Classe a construção do xml da NFSe
+ * para a Cidade de São Lourenço do Oeste SC
+ * conforme o modelo BETHA
+ *
+ * @category  NFePHP
+ * @package   NFePHP\NFSe\Counties\M4216909\Convert
+ * @copyright NFePHP Copyright (c) 2016
+ * @license   http://www.gnu.org/licenses/lgpl.txt LGPLv3+
+ * @license   https://opensource.org/licenses/MIT MIT
+ * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
+ * @author    Vagner Kaefer dos Santos <vagner at kaefer dot eng dot br>
+ * @link      http://github.com/nfephp-org/sped-nfse for the canonical source repository
+ */
+
+use NFePHP\NFSe\Models\Betha\Convert as ConvertModel;
+
+class Convert extends ConvertModel
+{
+}
---
+++ src/Counties/M4216909/Rps.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace NFePHP\NFSe\Counties\M4216909;
+
+/**
+ * Classe a construção do xml da NFSe para a
+ * Cidade de São Lourenço do Oeste SC
+ * conforme o modelo BETHA
+ *
+ * @category  NFePHP
+ * @package   NFePHP\NFSe\Counties\M4216909\Rps
+ * @copyright NFePHP Copyright (c) 2016
+ * @license   http://www.gnu.org/licenses/lgpl.txt LGPLv3+
+ * @license   https://opensource.org/licenses/MIT MIT
+ * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
+ * @author    Vagner Kaefer dos Santos <vagner at kaefer dot eng dot br>
+ * @link      http://github.com/nfephp-org/sped-nfse for the canonical source repository
+ */
+
+use NFePHP\NFSe\Models\Betha\Rps as RpsModel;
+
+class Rps extends RpsModel
+{
+
+}
